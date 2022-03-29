5d406c3d49d4118d5bc017be60a07d7941139dad
Anastasiia Dudchenko <anastasiia.dudchenko@onix-systems.com>
2022-03-29 15:29:30 +0300

    Initial commit

---
+++ app/src/main/java/com/simple/custom/slider/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.simple.custom.slider
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
